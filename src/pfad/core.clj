a5f1a94f606305f6b10d485a2cbf4fdafa59b824
Daniel Jay Haskin <djhaskin987@gmail.com>
2016-03-21 17:35:48 -0600

    Some comments

--- src/pfad/core.clj
+++ src/pfad/core.clj
@@ -1,15 +1,6 @@
 (ns pfad.core)
 
 
-(defn checklist [x]
-  (let [c (count x)
-        result (transient (vec (repeat c false)))]
-   (doseq [e x]
-     (when (< e c)
-       (assoc! result e true)))
-   (persistent! result)))
-
-
 (defn minfrom [a n x]
   (if (= 0 n)
     a
@@ -25,13 +16,25 @@
   [x]
   (minfrom 0 (count x) x))
 
+(defn checklist [x]
+  (let [c (count x)
+        result (transient (vec (repeat c false)))]
+   (doseq [e x]
+     (when (< e c)
+       (assoc! result e true)))
+   (persistent! result)))
+
 (defn search [x]
   (first (keep-indexed #(when (not %2) %1) x)))
 
+; Interestingly, the book says the other (divide/conquer alg) alg beats this
+; one by about 20% in Haskell.  In Clojure, I can use transients here (see
+; the checklist method). In benchmarks, this function *destroys* the other
+; one, something like a 2x increase in speed. It can also handle multiple
+; elements.
 (defn minfree-multi
   "Find the smallest natural number in a set,
   where the elements of the list are not guaranteed to be unique."
-
   [X]
   (let [search-result  (search (checklist X))]
     (if search-result
