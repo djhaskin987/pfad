92e15d909ad2429fb3d8396d670987e601da0141
Daniel Jay Haskin <djhaskin987@gmail.com>
2016-03-21 21:26:43 -0600

    Add

--- src/pfad/surpasser.clj
+++ src/pfad/surpasser.clj
@@ -1,4 +1,12 @@
 (ns pfad.surpasser)
 
+
+; This function is used everywhere in the pearl, so I'm just gonna define it here.
+(defn tails [sq]
+  (if (empty? sq)
+    sq
+    (cons [(first sq) (rest sq)] (tails (rest sq)))))
+
+
 (defn msc [sq] (count sq))
 
